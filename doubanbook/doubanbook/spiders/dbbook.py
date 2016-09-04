f3b76edd6208ccdc8543ae21b6eafc78eef16bc9
linmonsv <qin2@qq.com>
2016-09-04 14:12:59 +0800

    不小心又搬啦一次代码，，，

---
+++ doubanbook/doubanbook/spiders/dbbook.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+from doubanbook.items import DoubanbookItem
+
+class DbbookSpider(scrapy.Spider):
+    name = "dbbook"
+    #allowed_domains = ["https://www.douban.com/doulist/1264675/"]
+    start_urls = (
+        'https://www.douban.com/doulist/1264675//',
+    )
+
+    def parse(self, response):
+        #print(response.body)
+        item = DoubanbookItem()
+        selector = scrapy.Selector(response)
+        books = selector.xpath('//div[@class="bd doulist-subject"]')
+        for each in books:
+            title = each.xpath('div[@class="title"]/a/text()').extract()[0]
+            rate = each.xpath('div[@class="rating"]/span[@class="rating_nums"]/text()').extract()[0]
+            author = re.search('<div class="abstract">(.*?)<br', each.extract(), re.S).group(1)
+            title = title.replace(' ', '').replace('\n', '')
+            author = author.replace(' ', '').replace('\n', '')
+            item['title'] = title
+            item['rate'] = rate
+            item['author'] = author
+            #print('标题:' + title)
+            #print('评分:' + rate)
+            #print(author)
+            yield item
+            nextPage = selector.xpath('//span[@class="next"]/link/@href').extract()
+            if nextPage:
+                next = nextPage[0]
+                print(next)
+                yield scrapy.http.Request(next,callback=self.parse)
